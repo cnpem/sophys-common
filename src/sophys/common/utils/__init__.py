f11ec37704ea2020f45392683cd1aa255897aec6
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-03-12 10:50:14 -0300

    refactor!: remove databroker utility functions, keep only the built-in option
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/common/utils/__init__.py
+++ src/sophys/common/utils/__init__.py
@@ -11,7 +11,6 @@ from ophyd.areadetector.filestore_mixins import FileStoreHDF5IterativeWrite
 from .callbacks import *  # noqa: F403
 from .registry import *  # noqa: F403
 from .signals import *  # noqa: F403
-from .data_access import *  # noqa: F403
 
 
 class HDF5PluginWithFileStore(HDF5Plugin, FileStoreHDF5IterativeWrite):
