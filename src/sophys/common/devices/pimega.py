55a092c31d13334662ecaa64c0f15742f42b5f03
Rafael Lyra <rafael.lyra@lnls.br>
2024-05-15 15:16:46 -0300

    Remove pimega test timeout

--- src/sophys/common/devices/pimega.py
+++ src/sophys/common/devices/pimega.py
@@ -64,4 +64,3 @@ class PimegaDetector(DetectorBase):
 class Pimega(SingleTrigger, PimegaDetector):
     def __init__(self, name, prefix, **kwargs):
         super(Pimega, self).__init__(prefix, name=name, **kwargs)
-        self.wait_for_connection(timeout=10)
