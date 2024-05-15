1843ff7498c476ce66991b8ac838f6cd6fe6ca67
Rafael Lyra <rafael.lyra@lnls.br>
2024-05-15 14:51:58 -0300

    Export controllable motor device

--- src/sophys/common/devices/__init__.py
+++ src/sophys/common/devices/__init__.py
@@ -3,3 +3,4 @@ from .mobipix import Mobipix, MobipixEnergyThresholdSetter
 from .pilatus_300k import Pilatus
 from .vortex import Vortex
 from .pimega import Pimega
+from .motor import ControllableMotor
