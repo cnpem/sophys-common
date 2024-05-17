f45df6541623791826c8afaeb0ffe41ca72a4099
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-05-17 14:41:03 -0300

    refactor: sort imports in devices/__init__
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/common/devices/__init__.py
+++ src/sophys/common/devices/__init__.py
@@ -1,6 +1,6 @@
 from .c400 import C400, OldC400
 from .mobipix import Mobipix, MobipixEnergyThresholdSetter
-from .pilatus_300k import Pilatus
-from .vortex import Vortex
-from .pimega import Pimega
 from .motor import ControllableMotor
+from .pilatus_300k import Pilatus
+from .pimega import Pimega
+from .vortex import Vortex
