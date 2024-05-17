36d6a891ff8dbcd43b807c9cb1ec2ecc0a1f0b89
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-05-17 14:36:54 -0300

    feat: add channels and scales to the ERAS device
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/common/devices/eras.py
+++ src/sophys/common/devices/eras.py
@@ -1,9 +1,60 @@
-from ophyd import Component, Device, EpicsSignal, EpicsSignalRO, EpicsSignalNoValidation
+import copy
+
+from ophyd import Component, Device, EpicsSignal, EpicsSignalRO
 
 
 class ERAS(Device):
+    """Ophyd abstraction for the Ethernet Range Selector device."""
+
     device_name = Component(EpicsSignal, "GetDev", write_pv="SetDev")
     location = Component(EpicsSignal, "GetLoc", write_pv="SetLoc")
     version = Component(EpicsSignalRO, "GetVer")
 
-    reset = Component(EpicsSignalNoValidation, "Reset")
+    class Channel(Device):
+        """A channel from ERAS.
+
+        To control the currently selected scale, use `current_scale`.
+        You can use `scales[x]` or `scale_x` to access any scale at any time.
+        """
+
+        selected_scale = Component(EpicsSignal, "GetRng", write_pv="SetRng")
+        num_scales = Component(EpicsSignal, "GetNumScl", write_pv="SetNumScl")
+
+        @property
+        def current_scale(self):
+            self.selected_scale.wait_for_connection()
+
+            return self.scales[int(self.selected_scale.get())]
+
+        class Scale(Device):
+            full_scale_label = Component(EpicsSignal, "GetFSLbl", write_pv="SetFSLbl")
+            full_scale = Component(EpicsSignal, "GetFS", write_pv="SetFS")
+            sensitivity_label = Component(EpicsSignal, "GetSTLbl", write_pv="SetSTLbl")
+            sensitivity = Component(EpicsSignal, "GetST", write_pv="SetST")
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            if not hasattr(self.__class__, "_old_sig_attrs"):
+                self.__class__._old_sig_attrs = copy.deepcopy(self._sig_attrs)
+
+            self.scales = []
+            self._sig_attrs = self.__class__._old_sig_attrs
+
+            self.num_scales.wait_for_connection()
+            for i in range(int(self.num_scales.get())):
+                s = Component(self.Scale, "SC{}:".format(i))
+                s_name = "scale_{}".format(i)
+                s.__set_name__(s, s_name)
+
+                self._sig_attrs[s_name] = s
+                self._component_kinds[s_name] = s.kind
+                self._instantiate_component(s_name)
+
+                setattr(self, s_name, self._signals[s_name])
+                self.scales.append(self._signals[s_name])
+
+    channel_1 = Component(Channel, "CH1:")
+    channel_2 = Component(Channel, "CH2:")
+    channel_3 = Component(Channel, "CH3:")
+    channel_4 = Component(Channel, "CH4:")
