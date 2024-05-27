a4dfe8c4f82ad9aace7dc73fc06b7013e0300a6b
Rafael Lyra <rafael.lyra@lnls.br>
2024-05-27 14:49:38 -0300

    Add documentation

--- src/sophys/common/devices/motor.py
+++ src/sophys/common/devices/motor.py
@@ -17,10 +17,27 @@ class ControllableMotor(EpicsMotor):
         self.control_enabled.set(0)
 
 def VirtualControllableMotor(prefix, components, name, **kwargs):
+    """
+        Custom EpicsMotor that enables control of a list of motors 
+        before a plan and disables them after.
+        
+        This is useful for virtual motors that depends on the control state of several
+        real motors in order to work properly.
+
+        ```
+            componentsDict = {
+                "cnen_top": FormattedComponent(EpicsSignal, suffix='SWC:MOTOR:m2.CNEN'),
+                "cnen_bottom": FormattedComponent(EpicsSignal, suffix='SWC:MOTOR:m3.CNEN')
+            }
+            motor = VirtualControllableMotor("SWC:MOTOR:m1", componentsDict, "vertical_gap")
+        ```
+    """
+
     devClass = create_device_from_components(
         name="virtual_motor_class", base_class=EpicsMotor, **components)
     
     class VirtualControllableMotorClass(devClass):
+
         def __init__(self, attr_keys, **kwargs):
             super().__init__(**kwargs)
             self.attr_list = []
