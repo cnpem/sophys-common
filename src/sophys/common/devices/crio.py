96663d359e08ea440fdc53ebdb56029c07f49ea6
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-06-04 11:23:02 -0300

    feat: add PvAvgTime signal to CRIO 9215/9220
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/common/devices/crio.py
+++ src/sophys/common/devices/crio.py
@@ -1,8 +1,12 @@
-from ophyd import Component, EpicsSignal, EpicsSignalRO, Kind
+from ophyd import Component, Device, EpicsSignal, EpicsSignalRO, Kind
 from ophyd.device import create_device_from_components
 
 # Useful resources:
 # https://cnpemcamp.sharepoint.com/sites/lnls/groups/sol/SitePages/CRIO-PVs.aspx
+
+
+class _BaseCRIO(Device):
+    pv_averaging_time = Component(EpicsSignal, "PvAvgTime", kind=Kind.config)
 
 
 def __createAnalogIn(num: int) -> dict:
@@ -22,5 +26,9 @@ def __createAnalogIn(num: int) -> dict:
     return components
 
 
-CRIO_9215 = create_device_from_components("CRIO_9215", **__createAnalogIn(4))
-CRIO_9220 = create_device_from_components("CRIO_9220", **__createAnalogIn(16))
+CRIO_9215 = create_device_from_components(
+    "CRIO_9215", base_class=_BaseCRIO, **__createAnalogIn(4)
+)
+CRIO_9220 = create_device_from_components(
+    "CRIO_9220", base_class=_BaseCRIO, **__createAnalogIn(16)
+)
