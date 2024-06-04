59fbf88a1b22ade56445b365b48f2b3fb01e92d8
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-06-04 11:18:14 -0300

    feat: add offset and scale factor signals to CRIO 9215/9220
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/common/devices/crio.py
+++ src/sophys/common/devices/crio.py
@@ -1,9 +1,25 @@
-from ophyd import Component, EpicsSignalRO
+from ophyd import Component, EpicsSignal, EpicsSignalRO, Kind
 from ophyd.device import create_device_from_components
+
+# Useful resources:
+# https://cnpemcamp.sharepoint.com/sites/lnls/groups/sol/SitePages/CRIO-PVs.aspx
 
 
 def __createAnalogIn(num: int) -> dict:
-    return {f"ai{i}": Component(EpicsSignalRO, f"ai{i}") for i in range(num)}
+    components = {}
+    for i in range(num):
+        components.update(
+            {
+                f"ai{i}": Component(EpicsSignalRO, f"ai{i}", kind=Kind.hinted),
+                f"ai{i}_offset": Component(
+                    EpicsSignal, f"ai{i}_Offset", kind=Kind.config
+                ),
+                f"ai{i}_scale_factor": Component(
+                    EpicsSignal, f"ai{i}_SF", kind=Kind.config
+                ),
+            }
+        )
+    return components
 
 
 CRIO_9215 = create_device_from_components("CRIO_9215", **__createAnalogIn(4))
