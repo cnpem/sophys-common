e6070668c21e7c676d8e24cd54cfe68d398df33e
Rafael Lyra <rafael.lyra@lnls.br>
2024-06-24 17:19:54 -0300

    Change attribute names

--- src/sophys/common/devices/tatu.py
+++ src/sophys/common/devices/tatu.py
@@ -5,10 +5,10 @@ from ophyd import Component, FormattedComponent, DynamicDeviceComponent, \
 class TatuInput(Device):
     
     current_value = FormattedComponent(EpicsSignalRO, "{prefix}P{input_number}")
-    trigger_state = FormattedComponent(EpicsSignalRO, "{prefix}InputTriggerIO{input_number}")
-    edges_number_to_trigger = FormattedComponent(EpicsSignalRO, "{prefix}EdgestoTrigIO{input_number}")
-    readout_threshold = FormattedComponent(EpicsSignalRO, "{prefix}AnalogThresholdIO{input_number}")
-    associated_trigger_channel = FormattedComponent(EpicsSignalRO, "{prefix}AnalogAssocCh{input_number}")
+    trigger_value = FormattedComponent(EpicsSignalRO, "{prefix}InputTriggerIO{input_number}")
+    edges_to_trigger = FormattedComponent(EpicsSignalRO, "{prefix}EdgestoTrigIO{input_number}")
+    analog_threshold = FormattedComponent(EpicsSignalRO, "{prefix}AnalogThresholdIO{input_number}")
+    analog_assoc = FormattedComponent(EpicsSignalRO, "{prefix}AnalogAssocCh{input_number}")
 
     def __init__(self, prefix, input_number, **kwargs):
         self.input_number = input_number
@@ -36,9 +36,9 @@ class TatuOutput(Device):
     c1 = FormattedComponent(
         TatuOutputCondition, "{prefix}/{output_number}", condition_number="1")
     c2 = FormattedComponent(
-        TatuOutputCondition, suffix="{prefix}/{output_number}", condition_number="2")
+        TatuOutputCondition, "{prefix}/{output_number}", condition_number="2")
     c3 = FormattedComponent(
-        TatuOutputCondition, suffix="{prefix}/{output_number}", condition_number="3")
+        TatuOutputCondition, "{prefix}/{output_number}", condition_number="3")
 
     def __init__(self, prefix, output_number, **kwargs):
         self.output_number = output_number
@@ -119,7 +119,7 @@ class Tatu9401(TatuBase):
 
 class Tatu9403(TatuBase):
     """
-        
+
     """
 
     input2 = DynamicDeviceComponent({
