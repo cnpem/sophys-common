e654120cbf17fb006fbf23a136ecc8bddf733d2a
Rafael Lyra <rafael.lyra@lnls.br>
2024-06-27 10:46:22 -0300

    Add basic picolo device

---
+++ src/sophys/common/devices/picolo.py
@@ -0,0 +1,28 @@
+from ophyd import Device, Component, EpicsSignal, EpicsSignalWithRBV, \
+    DynamicDeviceComponent, EpicsSignalRO
+
+
+class PicoloChannel(Device):
+
+    enable = Component(EpicsSignal, "Enable")
+    value = Component(EpicsSignal, "EngValue", kind="hinted")
+    saturated = Component(EpicsSignal, "Saturated")
+    range = Component(EpicsSignalWithRBV, "Range")
+    auto_range = Component(EpicsSignal, "AutoRange")
+    acquire_mode = Component(EpicsSignalWithRBV, "AcquireMode")
+    state = Component(EpicsSignalRO, "State")
+
+
+class Picolo(Device):
+
+    range = Component(EpicsSignal, "Range")
+    auto_range = Component(EpicsSignal, "AutoRange")
+    acquisition_time = Component(EpicsSignalWithRBV, "AcquisitionTime")
+    sample_rate = Component(EpicsSignalWithRBV, "SampleRate")
+    acquire_mode = Component(EpicsSignal, "AcquireMode")
+    continuous_mode = DynamicDeviceComponent({
+        "ch1": (PicoloChannel, "Current1:"),
+        "ch2": (PicoloChannel, "Current2:"),
+        "ch3": (PicoloChannel, "Current1:"),
+        "ch4": (PicoloChannel, "Current2:")
+    })
