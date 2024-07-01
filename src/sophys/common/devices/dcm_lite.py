c7c9b75c75c83938dcd61bde250a83fec9af9e84
Rafael Lyra <rafael.lyra@lnls.br>
2024-07-01 15:10:30 -0300

    Fix minor syntax bugs

--- src/sophys/common/devices/dcm_lite.py
+++ src/sophys/common/devices/dcm_lite.py
@@ -7,7 +7,7 @@ class Goniometer(PVPositionerIsClose):
 
     readback = FormattedComponent(EpicsSignal, "{prefix}DCM01:GonRx{device_number}_SP_RBV", kind="hinted")
     setpoint = FormattedComponent(EpicsSignalRO, "{prefix}DCM01:GonRx{device_number}_SP", kind="config")
-    actuate = Component(EpicsSignal, "GonRxUpdate_SP", kind="omitted")
+    actuate = Component(EpicsSignal, "DCM01:GonRxUpdate_SP", kind="omitted")
 
     def __init__(self, prefix, device_number, **kwargs):
         self.device_number = device_number
@@ -16,9 +16,9 @@ class Goniometer(PVPositionerIsClose):
 
 class ShortStroke(PVPositionerIsClose):
 
-    readback = FormattedComponent(EpicsSignal, "{prefix}Shs{shs_axis}_S_RBV", kind="hinted")
-    setpoint = FormattedComponent(EpicsSignalRO, "{prefix}Shs{shs_axis}_SP", kind="config")
-    actuate = FormattedComponent(EpicsSignal, "ShsUpdate_{shs_axis}_SP", kind="omitted")
+    readback = FormattedComponent(EpicsSignal, "{prefix}DCM01:Shs{shs_axis}_S_RBV", kind="hinted")
+    setpoint = FormattedComponent(EpicsSignalRO, "{prefix}DCM01:Shs{shs_axis}_SP", kind="config")
+    actuate = FormattedComponent(EpicsSignal, "{prefix}DCM01:ShsUpdate_{shs_axis}_SP", kind="omitted")
 
     def __init__(self, prefix, shs_axis, **kwargs):
         self.shs_axis = shs_axis
@@ -27,12 +27,12 @@ class ShortStroke(PVPositionerIsClose):
 
 class DcmLite(Device):
 
-    gonio1 = Component(Goniometer, "1")
-    gonio2 = Component(Goniometer, "2")
+    gonio1 = Component(Goniometer, "", device_number="1")
+    gonio2 = Component(Goniometer, "", device_number="2")
     
-    gap = Component(ShortStroke, "Uy")
-    pitch = Component(ShortStroke, "Rx") 
-    roll = Component(ShortStroke, "Rz")
+    gap = Component(ShortStroke, "", shs_axis="Uy")
+    pitch = Component(ShortStroke, "", shs_axis="Rx") 
+    roll = Component(ShortStroke, "", shs_axis="Rz")
 
     leveler1 = Component(ControllableMotor, "PB01:m1")
     leveler2 = Component(ControllableMotor, "PB01:m2")
@@ -41,6 +41,6 @@ class DcmLite(Device):
 
     spindle_x_plus = Component(ControllableMotor, "PB01:m5")
     spindle_x_minus = Component(ControllableMotor, "PB01:m6")
-    
+
     granite_x = Component(ControllableMotor, "PB01:CS2:m7")
     granite_y = Component(ControllableMotor, "PB01:CS1:m8")
