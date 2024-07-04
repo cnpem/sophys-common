7e30b396707135e6c8eca6b72123e2b21c1d160c
Rafael Lyra <rafael.lyra@lnls.br>
2024-07-04 11:19:12 -0300

    Get enum float list in seconds

--- src/sophys/common/devices/picolo.py
+++ src/sophys/common/devices/picolo.py
@@ -43,7 +43,7 @@ class Picolo(Device):
 
     def set_acquisition_time(self, acquisiton_time: float):
         enums = self.acquisition_time.metadata['enum_strs']
-        enums_float = [float(item.replace(" ms", "")) for item in enums]
+        enums_float = [float(item.replace(" ms", ""))/1000 for item in enums]
         
         try:
             selected_enum = enums_float.index(acquisiton_time)
