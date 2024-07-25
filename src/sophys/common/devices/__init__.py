3507c729d0f5de12fedf58bdad6efee06cd0fbb1
Rafael Lyra <rafael.lyra@lnls.br>
2024-07-25 14:18:58 -0300

    Export PicoloDevice

--- src/sophys/common/devices/__init__.py
+++ src/sophys/common/devices/__init__.py
@@ -8,4 +8,5 @@ from .pimega import Pimega
 from .vortex import Vortex
 from .storage_ring import StorageRing
 from .tatu import Tatu9401, Tatu9403
+from .picolo import Picolo
 from .slit import VerticalSlit, HorizontalSlit, Slit
