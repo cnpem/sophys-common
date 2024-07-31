341c07189cbf75dfbf69c427140e3356c8fe1d25
Rafael Lyra <rafael.lyra@lnls.br>
2024-07-31 15:30:14 -0300

    Create APU 22 Device

--- src/sophys/common/devices/__init__.py
+++ src/sophys/common/devices/__init__.py
@@ -1,3 +1,4 @@
+from .ids import Apu22
 from .c400 import C400, OldC400
 from .crio import CRIO_9215, CRIO_9220
 from .eras import ERAS
---
+++ src/sophys/common/devices/ids.py
@@ -0,0 +1,14 @@
+from ophyd import EpicsSignal, EpicsSignalRO, Component, Device
+from ophyd.pv_positioner import PVPositionerIsClose
+
+class EpicsSignalIDs(PVPositionerIsClose):
+    setpoint = Component(EpicsSignal, "-SP")
+    readback = Component(EpicsSignalRO, "-Mon")
+
+class Apu22(Device):
+    kx = Component(EpicsSignalIDs, "Kx")
+    phase = Component(EpicsSignalIDs, "Phase")
+    phase_speed = Component(EpicsSignalIDs, "PhaseSpeed")
+    moving = Component(EpicsSignalRO, "DevCtrl-Cmd", string=True)
+    moving = Component(EpicsSignalRO, "Moving-Mon")
+    enabled = Component(EpicsSignalRO, "MotorsEnbld-Mon")
