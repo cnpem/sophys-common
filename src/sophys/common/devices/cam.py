fa2d8ad11fb80b18cd46aea40e089950df6f0e38
Rafael Lyra <rafael.lyra@lnls.br>
2024-08-02 10:25:54 -0300

    Create V33 of Xspress3DetectorCam
    V33 fixes removed PoolMaxBuffers PV

--- src/sophys/common/devices/cam.py
+++ src/sophys/common/devices/cam.py
@@ -1,5 +1,8 @@
-from ophyd.areadetector.cam import CamBase
+from ophyd.areadetector.cam import CamBase, Xspress3DetectorCam
 
 
 class CamBase_V33(CamBase):
+    pool_max_buffers = None
+
+class Xspress3DetectorCamV33(Xspress3DetectorCam):
     pool_max_buffers = None
