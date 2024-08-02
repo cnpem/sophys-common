c85eb9f467313fe39dbe0c1ad9b95d7b80f5d1bf
Rafael Lyra <rafael.lyra@lnls.br>
2024-08-02 17:06:53 -0300

    Add lazy parameters to PVs

--- src/sophys/common/devices/ocean.py
+++ src/sophys/common/devices/ocean.py
@@ -4,73 +4,73 @@ from ophyd import Component, Device, EpicsSignal, EpicsSignalRO, \
 
 class OceanInterestRegion(Device):
 
-    waveform = Component(EpicsSignal, "")
-    luminescence = Component(EpicsSignal, ":Luminescence")
-    upper_limit = Component(EpicsSignal, ":UpperLimit")
-    lower_limit = Component(EpicsSignal, ":LowerLimit")
+    waveform = Component(EpicsSignal, "", lazy=True)
+    luminescence = Component(EpicsSignal, ":Luminescence", lazy=True)
+    upper_limit = Component(EpicsSignal, ":UpperLimit", lazy=True)
+    lower_limit = Component(EpicsSignal, ":LowerLimit", lazy=True)
 
 
 class OceanIntegrationTime(Device):
 
-    resolution = Component(EpicsSignal, "Resolution")
-    driver_low = Component(EpicsSignal, "Value:DRVL")
-    driver_high = Component(EpicsSignal, "Value:DRVH")
-    value = Component(EpicsSignal, "Value")
-    calc = Component(EpicsSignal, "Calc")
+    resolution = Component(EpicsSignal, "Resolution", lazy=True)
+    driver_low = Component(EpicsSignal, "Value:DRVL", lazy=True)
+    driver_high = Component(EpicsSignal, "Value:DRVH", lazy=True)
+    value = Component(EpicsSignal, "Value", lazy=True)
+    calc = Component(EpicsSignal, "Calc", lazy=True)
 
 
 class OceanDetectorTemperature(PVPositionerIsClose):
     
-    readback = Component(EpicsSignalRO, "")
-    setpoint = Component(EpicsSignal, ":SetPoint")
-    actuate_value = Component(EpicsSignalRO, ":SetPoint:fbk")
+    readback = Component(EpicsSignalRO, "", lazy=True)
+    setpoint = Component(EpicsSignal, ":SetPoint", lazy=True)
+    actuate_value = Component(EpicsSignalRO, ":SetPoint:fbk", lazy=True)
 
 
 class OceanOpticsSpectrometer(Device):
 
-    index = Component(EpicsSignal, "SetIndex")
-    channel = Component(EpicsSignal, "SetChannel")
-    integration = Component(EpicsSignal, "SetIntegration")
-    averages = Component(EpicsSignal, "SetAverages")
-    boxcar = Component(EpicsSignal, "SetBoxcar")
+    index = Component(EpicsSignal, "SetIndex", lazy=True)
+    channel = Component(EpicsSignal, "SetChannel", lazy=True)
+    integration = Component(EpicsSignal, "SetIntegration", lazy=True)
+    averages = Component(EpicsSignal, "SetAverages", lazy=True)
+    boxcar = Component(EpicsSignal, "SetBoxcar", lazy=True)
 
-    spectra = Component(EpicsSignalRO, "Spectra")
-    dark_corrected_spectra = Component(EpicsSignalRO, "DarkCorrectedSpectra")
-    processed_spectra = Component(EpicsSignalRO, "Spectra:Processed")
-    spectra_axis = Component(EpicsSignalRO, "SpectraAxis")
+    spectra = Component(EpicsSignalRO, "Spectra", lazy=True)
+    dark_corrected_spectra = Component(EpicsSignalRO, "DarkCorrectedSpectra", lazy=True)
+    processed_spectra = Component(EpicsSignalRO, "Spectra:Processed", lazy=True)
+    spectra_axis = Component(EpicsSignalRO, "SpectraAxis", lazy=True)
 
-    electrical_dark = Component(EpicsSignal, "ElectricalDark")
-    dark_spectrum = Component(EpicsSignalRO, "GetDarkSpectrum")
-    external_trigger_mode = Component(
-        EpicsSignal, read_pv="ExternalTriggerMode", write_pv="ExternalTriggerMode:Set")
-    strobe = Component(EpicsSignal, "SetStrobe")
-    total_luminescence = Component(EpicsSignal, "TotalLuminescence")
-    acquisition_mode = Component(EpicsSignal, "AcquisitionMode")
-    acquire = Component(EpicsSignal, "Acquire")
-    acquiring = Component(EpicsSignalRO, "Acquiring")
+    electrical_dark = Component(EpicsSignal, "ElectricalDark", lazy=True)
+    dark_spectrum = Component(EpicsSignalRO, "GetDarkSpectrum", lazy=True)
+    external_trigger_mode = Component(EpicsSignalRO, "ExternalTriggerMode", lazy=True)
+    set_external_trigger_mode = Component(EpicsSignalRO, "ExternalTriggerMode:Set", lazy=True)
+    strobe = Component(EpicsSignal, "SetStrobe", lazy=True)
+    total_luminescence = Component(EpicsSignal, "TotalLuminescence", lazy=True)
+    acquisition_mode = Component(EpicsSignal, "AcquisitionMode", lazy=True)
+    acquire = Component(EpicsSignal, "Acquire", lazy=True)
+    acquiring = Component(EpicsSignalRO, "Acquiring", lazy=True)
 
-    region_full = Component(EpicsSignal, "RegionFull")
+    region_full = Component(EpicsSignal, "RegionFull", lazy=True)
 
-    region1 = Component(OceanInterestRegion, "Region1")
-    region2 = Component(OceanInterestRegion, "Region2")
-    region3 = Component(OceanInterestRegion, "Region3")
-    region4 = Component(OceanInterestRegion, "Region4")
-    region5 = Component(OceanInterestRegion, "Region5")
-
-
-    index = Component(EpicsSignal, "IntegrationTime:")
+    region1 = Component(OceanInterestRegion, "Region1", lazy=True)
+    region2 = Component(OceanInterestRegion, "Region2", lazy=True)
+    region3 = Component(OceanInterestRegion, "Region3", lazy=True)
+    region4 = Component(OceanInterestRegion, "Region4", lazy=True)
+    region5 = Component(OceanInterestRegion, "Region5", lazy=True)
 
 
-    external_trigger = Component(EpicsSignal, "ExternalTrigger")
-    external_trigger_invert = Component(EpicsSignal, "ExternalTrigger:Invert")
-    progress_bar = Component(EpicsSignal, "ProgressBar")
+    index = Component(OceanIntegrationTime, "IntegrationTime:", lazy=True)
 
-    file_path = Component(EpicsSignal, "fileDirectory")
-    file_root = Component(EpicsSignal, "fileRoot")
-    file_index = Component(EpicsSignal, "fileIndex")
 
-    script_path = Component(EpicsSignal, "scriptDirectory")
-    script_name = Component(EpicsSignal, "scriptName")
-    script_status = Component(EpicsSignal, "scriptStatus")
+    external_trigger = Component(EpicsSignal, "ExternalTrigger", lazy=True)
+    external_trigger_invert = Component(EpicsSignal, "ExternalTrigger:Invert", lazy=True)
+    progress_bar = Component(EpicsSignal, "ProgressBar", lazy=True)
 
-    det_temperature = Component(OceanDetectorTemperature, "DetectorTemp")    
+    file_path = Component(EpicsSignal, "fileDirectory", lazy=True)
+    file_root = Component(EpicsSignal, "fileRoot", lazy=True)
+    file_index = Component(EpicsSignal, "fileIndex", lazy=True)
+
+    script_path = Component(EpicsSignal, "scriptDirectory", lazy=True)
+    script_name = Component(EpicsSignal, "scriptName", lazy=True)
+    script_status = Component(EpicsSignal, "scriptStatus", lazy=True)
+
+    det_temperature = Component(OceanDetectorTemperature, "DetectorTemp", lazy=True)
