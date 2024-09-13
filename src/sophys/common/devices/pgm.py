617e933025f4ef81c31b258dea902010f9a6821f
Rafael Lyra <rafael.lyra@lnls.br>
2024-09-13 16:11:24 -0300

    Add PGM Device

---
+++ src/sophys/common/devices/pgm.py
@@ -0,0 +1,39 @@
+from ophyd.pv_positioner import PVPositionerIsClose
+from ophyd import Component, Device, EpicsSignal, EpicsSignalRO
+
+
+class PmacEpicsSignal(PVPositionerIsClose):
+
+    readback = Component(EpicsSignal, "SET")
+    setpoint = Component(EpicsSignalRO, "RBV")
+    sync = Component(EpicsSignal, "SYNC")
+
+
+class PGM(Device):
+    """
+        Device for controlling the PGM Monochromator.
+    """
+    
+    mode = Component(PmacEpicsSignal, "SCAN_MODE")
+    status = Component(PmacEpicsSignal, "SCAN_STATUS")
+    error_status = Component(PmacEpicsSignal, "SCAN_ERROR_STATUS")
+
+    csv_file_rows = Component(PmacEpicsSignal, "SCAN_CSV_FILE_ROWS")
+    csv_file_columns = Component(PmacEpicsSignal, "SCAN_CSV_FILE_COLUMNS")
+    csv_file_number = Component(PmacEpicsSignal, "SCAN_CSV_FILE_NUMBER")
+    
+    version = Component(PmacEpicsSignal, "SCAN_VERSION")
+    abort = Component(PmacEpicsSignal, "SCAN_ABORT")
+    
+    enable_int_gather = Component(PmacEpicsSignal, "SCAN_ENABLE_INT_GATHER")
+    enable_ext_gather = Component(PmacEpicsSignal, "SCAN_ENABLE_EXT_GATHER")
+    
+    step_exposure_time = Component(PmacEpicsSignal, "SCAN_STEP_EXPOSURE_TIME")
+    step_move_timeout = Component(PmacEpicsSignal, "SCAN_STEP_MOVE_TIMEOUT")
+    step_abort_on_timeout = Component(PmacEpicsSignal, "SCAN_STEP_ABORT_ON_TIMEOUT")
+    step_delay_to_trigger = Component(PmacEpicsSignal, "SCAN_STEP_DELAY_TO_TRIGGER")
+    
+    fly_trigger_out = Component(PmacEpicsSignal, "SCAN_FLY_TRIGGER_OUT")
+    
+    start = Component(EpicsSignal, "SendCmd")
+    servo_freq = Component(EpicsSignal, "SERVO_FREQ")
