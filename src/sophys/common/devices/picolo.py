ab20a19da3370125b0a983863ccc08dc7e0e561c
Rafael Lyra <rafael.lyra@lnls.br>
2024-09-13 14:46:02 -0300

    Add PicoloChannel Continuous acquisition PVs

--- src/sophys/common/devices/picolo.py
+++ src/sophys/common/devices/picolo.py
@@ -7,19 +7,24 @@ class PicoloChannel(Device):
     Device for one of the channels in the Picolo picoammeter.
     """
 
-    data = Component(EpicsSignalRO, "Data", kind="hinted")
-    enable = Component(EpicsSignal, "Enable", kind="config")
-    value = Component(EpicsSignal, "EngValue", kind="hinted")
-    saturated = Component(EpicsSignal, "Saturated", kind="config")
-    range = Component(EpicsSignalWithRBV, "Range", string=True, kind="config")
-    auto_range = Component(EpicsSignal, "AutoRange", kind="omitted")
-    acquire_mode = Component(EpicsSignalWithRBV, "AcquireMode", string=True, kind="config")
-    state = Component(EpicsSignalRO, "State", string=True, kind="config")
-    analog_bw = Component(EpicsSignalRO, "AnalogBW_RBV", kind="omitted")
+    data = Component(EpicsSignalRO, ":Data", kind="hinted")
+    scaled_data = Component(EpicsSignalRO, ":ScaledData", kind="hinted")
+
+    value = Component(EpicsSignalRO, "", kind="hinted")
+    scaled_value = Component(EpicsSignalRO, ":ScaledValue", kind="hinted")
+
+    enable = Component(EpicsSignal, ":Enable", kind="config")
+    engvalue = Component(EpicsSignal, ":EngValue", kind="hinted")
+    saturated = Component(EpicsSignal, ":Saturated", kind="config")
+    range = Component(EpicsSignalWithRBV, ":Range", string=True, kind="config")
+    auto_range = Component(EpicsSignal, ":AutoRange", kind="omitted")
+    acquire_mode = Component(EpicsSignalWithRBV, ":AcquireMode", string=True, kind="config")
+    state = Component(EpicsSignalRO, ":State", string=True, kind="config")
+    analog_bw = Component(EpicsSignalRO, ":AnalogBW_RBV", kind="omitted")
     
-    user_offset = Component(EpicsSignalWithRBV, "UserOffset", kind="config")
-    exp_offset = Component(EpicsSignalWithRBV, "ExpOffset", kind="config")
-    set_zero = Component(EpicsSignal, "SetZero", kind="omitted")
+    user_offset = Component(EpicsSignalWithRBV, ":UserOffset", kind="config")
+    exp_offset = Component(EpicsSignalWithRBV, ":ExpOffset", kind="config")
+    set_zero = Component(EpicsSignal, ":SetZero", kind="omitted")
 
 
 class PicoloAcquisitionTime(EpicsSignalWithRBV):
@@ -58,10 +63,10 @@ class Picolo(Device):
         "stop_acq": (EpicsSignal, "Stop", {})
     })
 
-    ch1 = Component(PicoloChannel, "Current1:")
-    ch2 = Component(PicoloChannel, "Current2:")
-    ch3 = Component(PicoloChannel, "Current3:")
-    ch4 = Component(PicoloChannel, "Current4:")
+    ch1 = Component(PicoloChannel, "Current1")
+    ch2 = Component(PicoloChannel, "Current2")
+    ch3 = Component(PicoloChannel, "Current3")
+    ch4 = Component(PicoloChannel, "Current4")
 
 
     def reset_data(self):
