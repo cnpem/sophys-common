f940044bec0a95a641b3026f8edcefb75c974218
Rafael Lyra <rafael.lyra@lnls.br>
2024-09-16 08:45:43 -0300

    Generalize the PGM Device Scan as PowerPmacScan

--- src/sophys/common/devices/__init__.py
+++ src/sophys/common/devices/__init__.py
@@ -14,4 +14,4 @@ from .picolo import Picolo
 from .slit import VerticalSlit, HorizontalSlit, Slit, KinematicSlit
 from .cam import CamBase_V33, Xspress3DetectorCamV33
 from .ocean import OceanOpticsSpectrometer
-from .pgm import Pgm
+from .power_pmac_scan import PowerPmacScan
