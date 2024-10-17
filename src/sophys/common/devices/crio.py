b42924e8b9d4c1b5759c0e0934eeb777b2da9c68
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-10-17 17:54:45 -0300

    fix: properly implement trigger logic for CRIO devices
    Because we have little control over the sampling period of CRIO devices,
    its trigger logic is a bit weird. We must wait a full `PvAvgTime` period
    before reading the device, because it is doing an acquisition.
    
    Furthermore, we often use CRIO devices with their individual signals, so
    we must take care to only wait that sampling period once for scan point,
    otherwise too much time would be lost.
    
    This uses a heuristic, in which a "scan point" is detected as not reading
    the same signal twice. When a repeated read is made, we assume it is
    referent to another scan point, and re-do the global trigger.
    
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/common/devices/crio.py
+++ src/sophys/common/devices/crio.py
@@ -1,3 +1,5 @@
+import time
+
 from ophyd import (
     Component,
     FormattedComponent as FormattedCpt,
@@ -7,6 +9,7 @@ from ophyd import (
     Kind,
 )
 from ophyd.device import create_device_from_components
+from ophyd.status import Status
 
 # Useful resources:
 # https://cnpemcamp.sharepoint.com/sites/lnls/groups/sol/SitePages/CRIO-PVs.aspx
@@ -23,13 +26,52 @@ class _BaseCRIO(Device):
 
         super().__init__(prefix, **kwargs)
 
+        self._seen_devices = set()
+
+    def cached_trigger(self, calling_child):
+        """Call `trigger` only once per `trigger_and_read` operation."""
+        # Only go forward if seen_devices is empty (first time here), or if
+        # calling_child is in seen_devices (another trigger_and_read pair).
+        # NOTE: This is so that we only trigger once for each point in a scan.
+        if len(self._seen_devices) != 0 and calling_child not in self._seen_devices:
+            self._seen_devices.add(calling_child)
+
+            sts = Status()
+            sts.set_finished()
+            return sts
+
+        self._seen_devices.clear()
+        self._seen_devices.add(calling_child)
+
+        return self.trigger()
+
+    def trigger(self):
+        sts = super().trigger()
+
+        # Restart averaging window
+        avg_time = self.pv_averaging_time.get()
+        # If too small for the firmware, it will use the smallest it can.
+        self.pv_averaging_time.set(0.001).wait()
+        self.pv_averaging_time.set(avg_time).wait()
+
+        time.sleep(avg_time)
+
+        return sts
+
+
+class BaseAnalogInput(EpicsSignalRO):
+    def trigger(self):
+        sts = super().trigger()
+        sts &= self.parent.cached_trigger(self)
+        return sts
+
 
 def __createAnalogIn(num: int) -> dict:
     components = {}
     for i in range(num):
         components.update(
             {
-                f"ai{i}": Component(EpicsSignalRO, f"ai{i}", kind=Kind.hinted),
+                f"ai{i}": Component(BaseAnalogInput, f"ai{i}", kind=Kind.hinted),
                 f"ai{i}_offset": Component(
                     EpicsSignal, f"ai{i}_Offset", kind=Kind.config
                 ),
