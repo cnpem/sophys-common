dd42dce4c304a7d86e2611dac9876a81c849a206
Rafael Lyra <rafael.lyra@lnls.br>
2025-02-05 12:52:32 -0300

    Add a parameterized number of MCA ROI

--- src/sophys/common/devices/xpress.py
+++ src/sophys/common/devices/xpress.py
@@ -1,19 +1,31 @@
 from ophyd import Component
 from ophyd.areadetector import DetectorBase, SingleTrigger
-from ..utils import HDF5PluginWithFileStore
+from ophyd.areadetector.plugins import ROIStatNPlugin_V25
+from ophyd.device import create_device_from_components
+from ..utils import HDF5PluginWithFileStoreV34
 from .cam import Xspress3DetectorCamV33
 
 
-class Xpress(SingleTrigger, DetectorBase):
-    hdf5 = Component(
-        HDF5PluginWithFileStore,
-        "HDF5:",
-        write_path_template="/tmp",
-        read_attrs=[],
-    )
+class XpressBase(SingleTrigger, DetectorBase):
+    pass
 
-    cam = Component(Xspress3DetectorCamV33, "det1:")
 
-    def __init__(self, *args, write_path=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.hdf5.write_path_template = write_path
+def Xpress(prefix: str, roi_num: int = 1, **kwargs):
+    xpressComponents = {
+        "hdf5": Component(
+            HDF5PluginWithFileStoreV34,
+            "HDF1:",
+            write_path_template="/tmp",
+            read_attrs=[]
+        ),
+        "cam": Component(Xspress3DetectorCamV33, "det1:")
+    }
+    for num in range(1, roi_num + 1):
+        for mca_num in range(1, 5):
+            xpressComponents.update({
+                f"mca{mca_num}roi_{num}": Component(ROIStatNPlugin_V25, f"MCA{mca_num}ROI:{num}:")
+            })
+
+    xpress = create_device_from_components(
+        name="xpress", base_class=XpressBase, **xpressComponents)
+    return xpress(prefix=prefix, **kwargs)
