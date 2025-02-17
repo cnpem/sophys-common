42969e1bd58d9579764537dd8d6a1694a275ffef
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2025-02-17 09:40:37 -0300

    fix: rename xpress -> xspress
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/common/devices/xspress.py
+++ src/sophys/common/devices/xspress.py
@@ -6,18 +6,18 @@ from ..utils import HDF5PluginWithFileStoreV34
 from .cam import Xspress3DetectorCamV33
 
 
-class XpressBase(SingleTrigger, DetectorBase):
+class XspressBase(SingleTrigger, DetectorBase):
     pass
 
 
-def Xpress(prefix: str, roi_num: int = 1, **kwargs):
-    xpressComponents = {
+def Xspress(prefix: str, roi_num: int = 1, **kwargs):
+    xspressComponents = {
         "hdf5": Component(HDF5PluginWithFileStoreV34, "HDF1:"),
         "cam": Component(Xspress3DetectorCamV33, "det1:"),
     }
     for num in range(1, roi_num + 1):
         for mca_num in range(1, 5):
-            xpressComponents.update(
+            xspressComponents.update(
                 {
                     f"mca{mca_num}roi_{num}": Component(
                         ROIStatNPlugin_V25, f"MCA{mca_num}ROI:{num}:"
@@ -25,7 +25,7 @@ def Xpress(prefix: str, roi_num: int = 1, **kwargs):
                 }
             )
 
-    xpress = create_device_from_components(
-        name="xpress", base_class=XpressBase, **xpressComponents
+    xspress = create_device_from_components(
+        name="xspress", base_class=XspressBase, **xspressComponents
     )
-    return xpress(prefix=prefix, **kwargs)
+    return xspress(prefix=prefix, **kwargs)
