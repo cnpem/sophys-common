e67351325c8576e7c8be6bf61d9e386c05856448
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2025-05-15 13:09:18 -0300

    test: add soft ioc for testing with EPICS connections
    This IOC is based off of the caproto example IOC "fake_motor_record" [1],
    with some alterations to e.g. support .CNEN on it.
    
    [1] - https://github.com/caproto/caproto/blob/3cdff5aaaf13e5718acfb9b2abf280e0bf76e6fe/caproto/ioc_examples/fake_motor_record.py
    
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,10 @@
 import pytest
+
+from .soft_ioc import start_soft_ioc
+
+
+@pytest.fixture(scope="session")
+def soft_ioc():
+    soft_ioc_prefix, stop_soft_ioc = start_soft_ioc()
+    yield soft_ioc_prefix
+    stop_soft_ioc()
