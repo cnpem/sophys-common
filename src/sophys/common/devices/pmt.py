014a3ee98a2ccd234049b3007c0bea55dcd11063
rafael.lyra <rafael.lyra@lnls.br>
2025-08-27 10:20:27 -0300

    Add more PVs to the photomultipler device

--- src/sophys/common/devices/pmt.py
+++ src/sophys/common/devices/pmt.py
@@ -8,7 +8,31 @@ class Photomultiplier(Device):
     """
 
     voltage = Component(EpicsSignalWithGetSet, "", ssuffix="VoltageDAC")
-    current = Component(EpicsSignalWithGetSet, "", ssuffix="CurrentDAC")
+    voltage_driver = Component(EpicsSignalWithGetSet, "", ssuffix="VoltageDriver")
     voltage_ramp = Component(EpicsSignalWithGetSet, "", ssuffix="VoltageRampDAC")
+
+    current = Component(EpicsSignalWithGetSet, "", ssuffix="CurrentDAC")
+    current_driver = Component(EpicsSignalWithGetSet, "", ssuffix="CurrentDriver")
+    current_driver_porc = Component(EpicsSignalWithGetSet, "", ssuffix="CurrentDriverPorc")
+
+    sample_holder_speed = Component(EpicsSignalWithGetSet, "", ssuffix="SampleHolderSpeed")
+    sample_holder_type = Component(EpicsSignalWithGetSet, "", ssuffix="SampleHolderType")
+
+    analog_input_ch1 = Component(EpicsSignalRO, "getAnalogInputCh1")
+    analog_input_ch2 = Component(EpicsSignalRO, "getAnalogInputCh2")
+    digital_input_ch1 = Component(EpicsSignalRO, "getDigitalInputCh1")
+    digital_input_ch2 = Component(EpicsSignalRO, "getDigitalInputCh2")
+    digital_output_ch1 = Component(EpicsSignalWithGetSet, "", ssuffix="DigitalOutputCh1")
+    digital_output_ch2 = Component(EpicsSignalWithGetSet, "", ssuffix="DigitalOutputCh2")
+    duty_cycle_ch1 = Component(EpicsSignalWithGetSet, "", ssuffix="DutyCycleCh1")
+    duty_cycle_ch2 = Component(EpicsSignalWithGetSet, "", ssuffix="DutyCycleCh2")
+
+    safety_mode = Component(EpicsSignalWithGetSet, "", ssuffix="SafetyMode")
+    frequency = Component(EpicsSignalWithGetSet, "", ssuffix="Frequency")
     status = Component(EpicsSignalRO, "Status")
     stop_pmt = Component(EpicsSignalRO, "Stop")
+
+    set_timer = Component(EpicsSignalRO, "setTimer")
+    read_timer = Component(EpicsSignalRO, "readTimer")
+    start_timer = Component(EpicsSignalRO, "startTimer")
+    stop_timer = Component(EpicsSignalRO, "stopTimer")
