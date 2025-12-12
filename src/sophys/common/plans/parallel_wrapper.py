2d355a61d88b0534fc0755c66294503a321837ac
rafael.lyra <rafael.lyra@lnls.br>
2025-12-12 15:15:41 -0300

    Run pre-commit

--- src/sophys/common/plans/parallel_wrapper.py
+++ src/sophys/common/plans/parallel_wrapper.py
@@ -6,9 +6,13 @@ from threading import Event, Thread
 async def wait_msg_to_complete(msg):
     await asyncio.wait_for(RE._command_registry[msg[0]](msg), timeout=10**10)
 
+
 def all_plans_have_finished(plan_has_finished_list):
-    return all(plan_finished.is_set() for plan_finished in plan_has_finished_list.values())
-    
+    return all(
+        plan_finished.is_set() for plan_finished in plan_has_finished_list.values()
+    )
+
+
 def parallel_plans_wrapper(*args):
     parallel_plan_list = list(args)
     plan_has_finished_list = {}
@@ -26,12 +30,14 @@ def parallel_plans_wrapper(*args):
         for list_id, _ in enumerate(parallel_plan_list):
             try:
                 if not plan_has_finished_list[list_id].is_set():
-                    if current_thread[list_id] == None:
+                    if current_thread[list_id] is None:
                         # Yield one message or yield it in a thread in case of a 'wait' message
                         current_event[list_id] = next(parallel_plan_list[list_id])
-                        if current_event[list_id][0] in ['wait', 'wait_for', 'sleep']:
+                        if current_event[list_id][0] in ["wait", "wait_for", "sleep"]:
                             current_thread[list_id] = Thread(
-                                target=asyncio.run, args=(wait_msg_to_complete(current_event[list_id]), ))
+                                target=asyncio.run,
+                                args=(wait_msg_to_complete(current_event[list_id]),),
+                            )
                             current_thread[list_id].start()
                         else:
                             yield current_event[list_id]
