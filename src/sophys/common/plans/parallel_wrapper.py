8d556cc172dcd2bf212b1750d9689716ac72f396
rafael.lyra <rafael.lyra@lnls.br>
2025-12-12 16:41:13 -0300

    Implement a single parallel loop manager for all the asynchronous coroutines

--- src/sophys/common/plans/parallel_wrapper.py
+++ src/sophys/common/plans/parallel_wrapper.py
@@ -1,10 +1,28 @@
-from bluesky import RunEngine as RE
 import asyncio
+from bluesky import RunEngine as RE
 from threading import Event, Thread
+from contextlib import contextmanager
 
 
 async def wait_msg_to_complete(msg):
     await asyncio.wait_for(RE._command_registry[msg[0]](msg), timeout=10**10)
+
+
+def run_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+
+@contextmanager
+def thread_loop_manager():
+    loop = asyncio.new_event_loop()
+    thread = Thread(target=run_loop, args=(loop,), daemon=True)
+    thread.start()
+    try:
+        yield loop
+    finally:
+        loop.call_soon_threadsafe(loop.stop)
+        thread.join()
 
 
 def all_plans_have_finished(plan_has_finished_list):
@@ -24,26 +42,32 @@ def parallel_plans_wrapper(*args):
         plan_has_finished_list[list_id] = Event()
         current_thread[list_id] = None
 
-    # Iterate through all the bluesky messages from all the plans
-    while not all_plans_have_finished(plan_has_finished_list):
-        for list_id, _ in enumerate(parallel_plan_list):
-            try:
-                if not plan_has_finished_list[list_id].is_set():
-                    if current_thread[list_id] is None:
-                        # Yield one message or yield it in a thread in case of a 'wait' message
-                        current_event[list_id] = next(parallel_plan_list[list_id])
-                        if current_event[list_id][0] in ["wait", "wait_for", "sleep"]:
-                            current_thread[list_id] = Thread(
-                                target=asyncio.run,
-                                args=(wait_msg_to_complete(current_event[list_id]),),
-                            )
-                            current_thread[list_id].start()
+    with thread_loop_manager() as loop:
+        # Iterate through all the bluesky messages from all the plans
+        while not all_plans_have_finished(plan_has_finished_list):
+            for list_id, _ in enumerate(parallel_plan_list):
+                try:
+                    if not plan_has_finished_list[list_id].is_set():
+                        if current_thread[list_id] is None:
+                            # Yield one message or yield it in a thread in case of a 'wait' message
+                            current_event[list_id] = next(parallel_plan_list[list_id])
+                            if current_event[list_id][0] in [
+                                "wait",
+                                "wait_for",
+                                "sleep",
+                            ]:
+                                current_thread[list_id] = (
+                                    asyncio.run_coroutine_threadsafe(
+                                        wait_msg_to_complete(current_event[list_id]),
+                                        loop,
+                                    )
+                                )
+                            else:
+                                yield current_event[list_id]
                         else:
-                            yield current_event[list_id]
-                    else:
-                        # Monitor 'wait' thread until its finished
-                        if not current_thread[list_id].is_alive():
-                            current_thread[list_id] = None
-            except Exception as exception:
-                plan_has_finished_list[list_id].set()
-                print(exception)
+                            # Monitor 'wait' thread until its finished
+                            if not current_thread[list_id].done():
+                                current_thread[list_id] = None
+                except Exception as exception:
+                    plan_has_finished_list[list_id].set()
+                    print(exception)
