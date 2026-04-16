bb34297f17da2c3aadd39fd5014c593ff5d074cb
João Luis Biondo Neto <joao.neto@lnls.br>
2026-04-16 09:35:03 -0300

    feat: add first idea for `MFC` device.
    `MFC` stands for Mass Flow Controller and it controls the flow rates within a gas/liquid system.

---
+++ src/sophys/common/devices/mfc.py
@@ -0,0 +1,68 @@
+from ophyd import Device, Component, EpicsSignalWithRBV, EpicsSignalRO, EpicsSignal
+from bluesky.utils import FailedStatus
+from sophys.common.utils.status import PremadeStatus
+from enum import StrEnum
+
+
+class MixFractionType(StrEnum):
+    VOLUME_FRACTION = "Volume fraction"
+    MASS_FRACTION = "Mass fraction"
+    MOLE_FRACTION = "Mole fraction"
+
+
+class InitResetType(StrEnum):
+    UNLOCKED = "unlocked"
+    LOCKED = "locked"
+
+
+class MFCFlowValueError(FailedStatus):
+    def __init__(self, value):
+        super().__init__(
+            f"The flow value {value} is out of range. The allowed range is between 0 and 32000"
+        )
+
+
+class MFCMixFluid(Device):
+    fluid_name_list = Component(
+        EpicsSignal, "FluidNameList", string=True, kind="config"
+    )
+    fluid_name = Component(EpicsSignalRO, "FluidName_RBV", string=True, kind="config")
+    fraction_type = Component(
+        EpicsSignalWithRBV, "FractionType", string=True, kind="config"
+    )
+    fraction = Component(EpicsSignalWithRBV, "Fraction", kind="config")
+
+
+class MFCFlow(EpicsSignalWithRBV):
+    def set(self, value, *args, **kwargs):
+        if 0 <= value <= 32000:  # check for the allowed dimensionless flow range
+            return super().set(value, *args, **kwargs)
+        else:
+            return PremadeStatus(success=False, exception=MFCFlowValueError(value))
+
+
+class MFC(Device):
+    setpoint = Component(MFCFlow, "Setpoint", kind="config")
+    readback = Component(
+        EpicsSignalRO, "Measure_RBV", kind="hinted"
+    )  # Dimensionless metered value of flow
+
+    fmeasure = Component(EpicsSignalRO, "FloatMeasure_RBV", kind="hinted")
+    fsetpoint = Component(EpicsSignalWithRBV, "FloatSetpoint", kind="config")
+    capacity = Component(EpicsSignalWithRBV, "Capacity", kind="config")  #
+    capacity_unit = Component(
+        EpicsSignalWithRBV, "CapacityUnit", string=True, kind="config"
+    )
+
+    fluid_name = Component(EpicsSignalRO, "FluidName_RBV", string=True, kind="config")
+    fluid_name_list = Component(
+        EpicsSignal, "FluidNameList", string=True, kind="config"
+    )
+    mix = Component(MFCMixFluid, "Mix", kind="config")
+
+    inlet_pressure = Component(EpicsSignalWithRBV, "InletPressure", kind="config")
+    outlet_pressure = Component(EpicsSignalWithRBV, "OutletPressure", kind="config")
+
+    temperature = Component(EpicsSignalRO, "Temperature_RBV", kind="config")
+
+    init_reset = Component(EpicsSignalWithRBV, "InitReset", string=True, kind="config")
